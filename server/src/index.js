543d3a35a20b176b3dddc18bf1ff280cbe905a55
Franco Sayago <franco.sayago-contractor@fabfitfun.com>
2024-08-30 20:52:35 -0300

    Refresh en produccion y current month en reportes

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,28 @@
-import "dotenv/config"; // Para cargar la variable del .env
+import 'dotenv/config'; // Para cargar la variable del .env
 
-import app from "./app.js";
-import { PORT } from "./config.js";
-import { connectDB } from "./db.js";
+import app from './app.js';
+import { PORT } from './config.js';
+import { connectDB } from './db.js';
 
 export default async function main() {
-    try {
-        // Conectar a la base de datos
-        connectDB().then(() => {
-            // Iniciar el servidor solo después de que la conexión a la base de datos esté establecida
-            app.listen(PORT, () => {
-                console.log(`Server running on port ${PORT}`);
-            });
-        });
-    } catch (error) {
-        console.error("Error starting the server:", error);
-    }
+  try {
+    // Conectar a la base de datos
+    connectDB().then(() => {
+      // Iniciar el servidor solo después de que la conexión a la base de datos esté establecida
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    });
+    app.get('/*', function (req, res) {
+      res.sendFile(path.join(__dirname, ''), function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      });
+    });
+  } catch (error) {
+    console.error('Error starting the server:', error);
+  }
 }
 
 main();
